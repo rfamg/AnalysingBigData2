5b4afd6e234ad478438dfddc9b271456dc97398c
rfamg <rafaelguimaraes@Rafaels-MacBook-Pro.local>
2017-03-27 23:06:42 -0400

    ok

---
+++ game.R
@@ -0,0 +1,117 @@
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+#  A Game of Two Halves 
+#  Written by Rafael Guimaraes,  Yuanxi Yao and Yilan Tang
+#  Date March 27th 2017
+#  Version 1.0
+#  ---------------------------------------------------------------------------
+#  ---------------------------------------------------------------------------
+
+
+#  ---------------------------------------------------------------------------
+#  Library
+#  ---------------------------------------------------------------------------
+
+library(fBasics) # Show basic aspect of the data
+library(readr)  # provide additional data-reading functions
+library(corrplot) # attractive correlation graphs
+library(ggplot2) # for plots
+library(data.table)
+library(dplyr)
+library(reshape2)
+library(corrgram)
+library(nnet)
+library(party)
+
+#  ---------------------------------------------------------------------------
+#  Data understanding
+#  ---------------------------------------------------------------------------
+
+
+
+#uploading data
+
+df.train <- read.csv("~/Desktop/Brandes/Analysing Big Data 2/Analysing Big Data 2/data/Betting.csv")
+df.test <- read.csv("~/Desktop/Brandes/Analysing Big Data 2/Analysing Big Data 2/data/BettingTest.csv", header=T)
+
+#Read file
+#mydata<-read.csv("~/Desktop/Brandes/Analysing Big Data 2/Analysing Big Data 2/data/Betting.csv",header=T)
+
+#  ---------------------------------------------------------------------------
+#  Train data
+#  ---------------------------------------------------------------------------
+
+# First the multinomial Logit model
+
+
+
+df.train$prog2<-relevel(df.train$Match_O, ref="Loss")
+train<-multinom(prog2~HTGD+RED.H+RED.A+POINTS_H+POINTS_A+TOTAL_H_P+TOTAL_A_P+FGS.0+FGS.1, 
+               data=df.train)
+summary(train)
+
+#2-tailed z test
+z <- summary(train)$coefficients/summary(train)$standard.errors
+p <- (1 - pnorm(abs(z), 0, 1)) * 2
+p
+
+t.test(df.train$RED.H, df.train$RED.A)
+
+#Exponentiate
+exp(coef(train))
+
+#Decision tree with party
+# First install and invoke party
+
+
+datactree <- ctree(Match_O~HTGD+RED.H+RED.A+POINTS_H+POINTS_A+TOTAL_H_P+TOTAL_A_P+FGS.0+FGS.1, 
+                   df.train, controls=ctree_control(mincriterion=0.9, minsplit=50))
+print(datactree)
+plot(datactree,type="simple")
+
+#Misclassification error  
+tab<-table(predict(datactree), train$Match_O)
+print(tab)
+1-sum(diag(tab))/sum(tab)
+
+
+#  ---------------------------------------------------------------------------
+#  test data
+#  ---------------------------------------------------------------------------
+
+# First the multinomial Logit model
+
+names(test)
+
+df.test$prog2<-relevel(df.test$MATCH_O, ref="Loss")
+test<-multinom(prog2~HTGD+RED_H+RED_A+POINTS_H+POINTS_A+TOTAL_H_P+TOTAL_A_P+FGS.0+FGS.1, 
+                data=df.test)
+summary(test)
+
+#2-tailed z test
+a <- summary(test)$coefficients/summary(test)$standard.errors
+b <- (1 - pnorm(abs(z), 0, 1)) * 2
+b
+
+#Exponentiate
+exp(coef(test))
+
+
+#Decision tree with party
+# First install and invoke party
+
+
+datactree2 <- ctree(MATCH_O~HTGD+RED_H+RED_A+POINTS_H+POINTS_A+TOTAL_H_P+TOTAL_A_P+FGS.0+FGS.1, 
+                   df.test, controls=ctree_control(mincriterion=0.5, minsplit=20))
+print(datactree2)
+plot(datactree2,type="simple")
+
+#Misclassification error  
+tab2<-table(predict(datactree2), df.test$MATCH_O)
+print(tab2)
+1-sum(diag(tab2))/sum(tab2)
+
+
+
+
+
